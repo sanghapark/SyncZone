331b035eca4a69a2dd13197b9a85e7c15f4f149d
Sang-Ha Park <sanghapark1021@gmail.com>
2017-03-18 12:04:13 +0900

    initial commit

---
+++ SyncZone/SyncZone.h
@@ -0,0 +1,19 @@
+//
+//  SyncZone.h
+//  SyncZone
+//
+//  Created by ParkSangHa on 2017. 3. 18..
+//  Copyright © 2017년 sanghapark1021. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SyncZone.
+FOUNDATION_EXPORT double SyncZoneVersionNumber;
+
+//! Project version string for SyncZone.
+FOUNDATION_EXPORT const unsigned char SyncZoneVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SyncZone/PublicHeader.h>
+
+
